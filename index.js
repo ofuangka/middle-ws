6b583702d63cb547b393e1a60058b28c8ece0d9f
Osha Fuangkasae <ofuangka@gmail.com>
2016-04-16 19:51:02 -0400

    manage groups in index.js

--- index.js
+++ index.js
@@ -1,8 +1,74 @@
-var WebSocketServer = require('ws').Server;
-var port = process.env.PORT || 5000;
+(function wrapper() {
+    var WebSocketServer = require('ws').Server;
+    var port = process.env.PORT || 5000;
+    var server = new WebSocketServer({
+        port: port
+    }, function callback() {
+        // do nothing
+    });
 
-var wss = new WebSocketServer({
-    port: port
-}, function callback() {
-    console.log(arguments);
-});
+    var groups = {};
+
+    server.on('connection', function socketDidConnect(socket) {
+        function removeSocketFromGroups() {
+            var i, len, groupId, member;
+
+            for (groupId in groups) {
+                for (i = 0, len = groups[groupId].length; i < len; i++) {
+                    member = groups[groupId][i];
+                    if (member === socket) {
+
+                        /* remove the member from the group */
+                        groups[groupId].splice(i, 1);
+
+                        /* remove the group if it is now empty */
+                        if (groups[groupId].length === 0) {
+                            delete groups[groupId];
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        /* each message is considered to be a group join. */
+        socket.on('message', function socketDidSendMessage(message, flags) {
+            function isValid(data) {
+                return true;
+            }
+
+            var groupId, i, len;
+            if (isValid(message)) {
+                groupId = message.groupId;
+
+                /* we consider this to be a new group join, so remove the socket from previous groups */
+                removeSocketFromGroups();
+
+                /* create the group if it does not yet exist */
+                if (!(groupId in groups)) {
+                    groups[groupId] = [];
+                }
+
+                /* add the socket to the group */
+                groups[groupId].push(socket);
+
+                /* notify all the group members of the event */
+                for (i = 0, len = groups[groupId].length; i < len; i++) {
+                    groups[groupId][i].send(JSON.stringify({
+                        groupId: groupId,
+                        userId: message.userId,
+                        lat: message.lat,
+                        lng: message.lng
+                    }));
+                }
+
+            } else {
+                /* the message wasn't valid, so don't do anything with it */
+            }
+
+        });
+        socket.on('close', function socketDidClose() {
+            removeSocketFromGroups();
+        });
+    });
+}());
